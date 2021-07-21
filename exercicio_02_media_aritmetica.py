91449e04a74e3de144299a8d8da75812a6dd015a
Bruno Dias Cultural <83181304+brunodiascultural@users.noreply.github.com>
2021-07-21 15:52:15 -0300

    Add files via upload

---
+++ exercicio_02_media_aritmetica.py
@@ -0,0 +1,7 @@
+nota4 = float(input("Digite a primeira nota4: "))
+nota5 = float(input("Digite a segunda nota5: "))
+nota6 = float(input("Digite a terceira nota6: "))
+nota7 = float(input("Digite a quarta nota7: "))
+média = (nota4 + nota5 + nota6 + nota7) / 4
+print( "A média aritmética é", média)
+
